a5af25a978982392cfd7c7b87c48e5a3c9c39abe
Benjamin <benjamin.gaymay@epitech.eu>
2024-07-11 14:36:48 +0200

    reset

--- js/components/scene.js
+++
@@ -1,8 +0,0 @@
-import { Scene, Color } from '../lib/build/three.module.js';
-
-export function createScene() {
-	const scene = new Scene();
-	scene.background = new Color('black');
-
-	return scene;
-}
